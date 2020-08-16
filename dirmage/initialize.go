563a5d90a108332924a51131d3c5ce5e866e7c22
Y-Ysss <y-ysss@outlook.com>
2020-08-16 20:37:01 +0900

    Update(initialize.go, config.go): Improved the implementation to be easier to use.

--- dirmage/initialize.go
+++ dirmage/initialize.go
@@ -1,23 +1,37 @@
 package dirmage
 
 import (
-	"os"
+	"encoding/json"
+	"fmt"
 	"log"
+	"os"
+
 	"github.com/BurntSushi/toml"
 )
 
+var dirsList map[string][]dirInfo = make(map[string][]dirInfo)
+var conf Config
+
 func Initialize() {
-	var conf Config
-    if _, err := toml.DecodeFile("config.toml", &conf); err != nil {
-    	log.Fatal(err)
+	if _, err := toml.DecodeFile("config.toml", &conf); err != nil {
+		log.Fatal(err)
 	}
-    if(exists(conf.Selector.DirsListPath)) {
-    	DirectoriesList = conf.Selector.DirsListPath
-    }
-    PromptString = conf.Prompt.String
+	fmt.Println(conf)
+	if !fileExists(conf.Data.DirsFile) {
+		conf.Data.DirsFile = "directories.json"
+	}
+
+	readDrectoriesJson()
 }
 
-func exists(filename string) bool {
-    _, err := os.Stat(filename)
-    return err == nil
+func fileExists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
+}
+
+func readDrectoriesJson() {
+	dirsJson := ReadFile(conf.Data.DirsFile)
+	if unmsErr := json.Unmarshal(([]byte)(dirsJson), &dirsList); unmsErr != nil {
+		log.Fatal(unmsErr)
+	}
 }
